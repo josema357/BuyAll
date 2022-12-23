b6a067647d3fd42d76c6697fbbfc0bef70387367
Jose Manuel <j.ochoa317@gmail.com>
2022-12-23 11:06:36 -0500

    UI index - ShoppingCart

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ const menuEmail=document.querySelector(".navbar-email");
 const desktopMenu=document.querySelector(".desktop-menu")
 const menuHamIcon=document.querySelector(".menu");
 const mobileMenu=document.querySelector(".mobile-menu");
+const menuCartIcon=document.querySelector(".navbar-shopping-cart");
+const aside=document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click",toggleDesktopMenu);
 menuHamIcon.addEventListener("click",toggleMobileMenu);
+menuCartIcon.addEventListener("click",toggleCarritoAside);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+    aside.classList.add("inactive");
 }
 
 function toggleMobileMenu(){
-    console.log("click")
     mobileMenu.classList.toggle("inactive");
+    aside.classList.add("inactive");
+}
+
+function toggleCarritoAside(){
+    mobileMenu.classList.add("inactive");
+    desktopMenu.classList.add("inactive");
+    aside.classList.toggle("inactive");
 }
